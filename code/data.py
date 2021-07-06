5b423710ac24b43c1ce9260e52ff12356fb71eb0
CalvinMT <calvin.massonnet@gmail.com>
2021-07-06 10:24:08 +0200

    Feature auto-calculate mfcc's nfft

--- code/data.py
+++ code/data.py
@@ -5,9 +5,60 @@ from __future__ import division
 import re, os
 import numpy as np
 from collections import Counter
+from python_speech_features import fbank, lifter
 from python_speech_features import delta
-from python_speech_features import mfcc
+from scipy.fftpack import dct
 import scipy.io.wavfile as wav
+
+def calculate_nfft(samplerate, winlen):
+    """
+    source : https://github.com/jameslyons/python_speech_features/pull/76/files
+
+    Calculates the FFT size as a power of two greater than or equal to
+    the number of samples in a single window length.
+    
+    Having an FFT less than the window length loses precision by dropping
+    many of the samples; a longer FFT than the window allows zero-padding
+    of the FFT buffer which is neutral in terms of frequency domain conversion.
+    :param samplerate: The sample rate of the signal we are working with, in Hz.
+    :param winlen: The length of the analysis window in seconds.
+    """
+    window_length_samples = winlen * samplerate
+    nfft = 1
+    while nfft < window_length_samples:
+        nfft *= 2
+    return nfft
+
+def mfcc(signal, samplerate=16000, winlen=0.025, winstep=0.01, numcep=13,
+         nfilt=26, nfft=None, lowfreq=0, highfreq=None, preemph=0.97, ceplifter=22,
+         appendEnergy=True, winfunc=lambda x:np.ones((x,))):
+    """
+    source : https://github.com/jameslyons/python_speech_features/pull/76/files
+
+    Compute MFCC features from an audio signal.
+
+    :param signal: the audio signal from which to compute features. Should be an N*1 array
+    :param samplerate: the sample rate of the signal we are working with, in Hz.
+    :param winlen: the length of the analysis window in seconds. Default is 0.025s (25 milliseconds)
+    :param winstep: the step between successive windows in seconds. Default is 0.01s (10 milliseconds)
+    :param numcep: the number of cepstrum to return, default 13
+    :param nfilt: the number of filters in the filterbank, default 26.
+    :param nfft: the FFT size. Default is None, which uses the calculate_nfft function to choose the smallest size that does not drop sample data.
+    :param lowfreq: lowest band edge of mel filters. In Hz, default is 0.
+    :param highfreq: highest band edge of mel filters. In Hz, default is samplerate/2
+    :param preemph: apply preemphasis filter with preemph as coefficient. 0 is no filter. Default is 0.97.
+    :param ceplifter: apply a lifter to final cepstral coefficients. 0 is no lifter. Default is 22.
+    :param appendEnergy: if this is true, the zeroth cepstral coefficient is replaced with the log of the total frame energy.
+    :param winfunc: the analysis window to apply to each frame. By default no window is applied. You can use numpy window functions here e.g. winfunc=numpy.hamming
+    :returns: A numpy array of size (NUMFRAMES by numcep) containing features. Each row holds 1 feature vector.
+    """
+    nfft = nfft or calculate_nfft(samplerate, winlen)
+    feat,energy = fbank(signal,samplerate,winlen,winstep,nfilt,nfft,lowfreq,highfreq,preemph,winfunc)
+    feat = np.log(feat)
+    feat = dct(feat, type=2, axis=1, norm='ortho')[:,:numcep]
+    feat = lifter(feat,ceplifter)
+    if appendEnergy: feat[:,0] = np.log(energy) # replace first cepstral coefficient with log of frame energy
+    return feat
 
 def build_training_list(path, testing_list, validation_list):
     """
